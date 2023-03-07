8657038df8e437fda9c2aebb90083259e875668c
samsontaiwo <tywo389@gmail.com>
2023-03-07 11:19:13 -0600

    login: changes made

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -1,26 +1,38 @@
 import React from 'react'
+import { faEye } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import type { Dispatch, SetStateAction} from 'react';
 
-type LoginValues = {
-    firstName: string;
-    lastName: string;
-    email: string;
-}
 
-const Login = () => {
+
+const CreateUser = ({passwordVisibility, setPasswordVisibility} : {passwordVisibility: boolean; setPasswordVisibility : Dispatch<SetStateAction<boolean>>}) => {
+
+    const handlePasswordVisibility = () => {
+        setPasswordVisibility(!passwordVisibility)
+    }
+
+
     return (
         <div className= 'login-page'>
             <form>
-                <label htmlFor='firstName'>First Name: </label>
-                <input type='text' placeholder='Enter Your First Name...'/>
-                <br/>
-                <label htmlFor='lastname'>Last Name: </label>
-                <input type='text' placeholder='Enter Your Last Name...'/>
-                <br/>
-                <label htmlFor='emailAddress'>Email Address</label>
-                <input type='email'/> 
-                <br/>
-                <label htmlFor='password'>Password</label>
-                <input type='password'/> <br/>
+                <section>
+                    <label htmlFor='firstName'>First Name: </label>
+                    <label htmlFor='lastname'>Last Name: </label>
+                </section>
+                <section>
+                    <input className='loginInput'type='text' placeholder='First Name'/>
+                    <input type='text' placeholder='Last Name'/>
+                </section>
+                <section>
+                    <label htmlFor='email-password'>
+                        Email Address <br/>
+                        <input type='email' placeholder='Enter your email'/> <br/>
+                        Password  <br/>
+                        <input type={passwordVisibility ? 'password' : 'text'} placeholder='Enter new password'/> 
+                        <button className='password-toggle' onClick={()=>handlePasswordVisibility()}><FontAwesomeIcon icon={faEye}/></button>
+                        <br/>     
+                    </label> <br/>    
+                </section>
                 <button>Continue to ChatR</button>
             </form>
             <span>Already have an account?<a href='notworkingyet.html'>Login here</a></span>
@@ -28,4 +40,4 @@ const Login = () => {
     )
 }
 
-export default Login
+export default CreateUser
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,11 +1,14 @@
 import Layout from '~/components/Layout';
 import Login from '~/components/Login'
+import {useState} from 'react'
 
 const Index = () => {
+  const [userInfo, setUserInfo] = useState([])
+  const [passwordVisibility, setPasswordVisibility] = useState<boolean>(true);
   return (
     <Layout>
       <p>hello chat</p>
-      <Login/>
+      <Login passwordVisibility={passwordVisibility} setPasswordVisibility={setPasswordVisibility}/>
     </Layout>
   );
 };
